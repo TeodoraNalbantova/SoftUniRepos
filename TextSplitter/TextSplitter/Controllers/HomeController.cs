e907db87f8c0c52a0efe096e3ef9ba6ee77c5580
TeodoraNalbantova <teodoranalbantova88@gmail.com>
2024-03-06 13:41:38 +0200

    ASP.NET Core VMC

---
+++ TextSplitter/TextSplitter/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace TextSplitter.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextSplitterViewModel textViewModel)
+        {
+            return View(textViewModel);
+        }
+        [HttpPost]
+        public IActionResult Split(TextSplitterViewModel textViewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return this.RedirectToAction("Index", textViewModel);
+
+            }
+
+            string[] words = textViewModel.TextToSplit.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string splitText = String.Join(Environment.NewLine, words);
+
+            textViewModel.SplitText = splitText;
+
+            return this.RedirectToAction("Index", textViewModel);
+        }
+
+       
+
+    }
+}
---
+++ TextSplitter/TextSplitter/ViewModels/TextSplitterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitter.ViewModels
+{
+    public class TextSplitterViewModel
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "The field Text must be a string with a minimum length of 2 and maximum length of 30.")]
+        public string TextToSplit { get; set; } = null!;
+
+        public string? SplitText { get; set; }
+    }
+}
